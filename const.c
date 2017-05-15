ec59aa4cd9172f48dd5032588da81d535325203b
Pedro Henrique - A77377 <a77377@alunos.uminho.pt>
2017-05-15 04:26:49 +0100

    Feita uma primeira versão de window e aumentado o tamanho do buffer em const.c para PIPE_BUF.

--- const.c
+++ const.c
@@ -3,31 +3,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <limits.h>
 /*
 argv[0] = const
 argv[1] = valor
 nota: em vez de ler 256, tamanho vai ser PIPE_BUF
 */
 int main(int argc, char** argv){
-int x;
-char buffer[256];
 
-	if(argc == 2){
-    	
-		while( (( x = read(0,buffer,256)) > 0)){
-			strcpy(&buffer[x-1], ":");
-			strcat(buffer, argv[1]);
-			strcat(buffer, "\n");
+		int x;
+		char buffer[PIPE_BUF];
 
-			write(1,buffer,strlen(buffer));
-		
+		if (argc == 2) {
+
+			while (((x = read(0,buffer,PIPE_BUF)) > 0)) {
+				strcpy(&buffer[x-1], ":");
+				strcat(buffer, argv[1]);
+				strcat(buffer, "\n");
+
+				write(1,buffer,strlen(buffer));
+
+			}
 		}
-	}
 
-	else{
-		write(1, "Too many or very few arguments!\n", 32);
-		return EXIT_FAILURE;
-	}
+		else {
+			write(1, "Too many or very few arguments!\n", 32);
+			return EXIT_FAILURE;
+		}
 
-return EXIT_SUCCESS;
+		return EXIT_SUCCESS;
 }
---
+++ window.c
@@ -0,0 +1,141 @@
+#include <unistd.h> /* chamadas ao sistema: defs e decls essenciais */
+#include <sys/wait.h> /* chamadas wait*() e macros relacionadas */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+
+int getVal(char *line, int coluna, long *val){
+
+    int two_dots = 0, reachEnd = 0, idx, i;
+	  int size = strlen(line);
+    char saved_elem[21];   /*20 dígitos do maior unsigned int representável em 64 bits mais 1 do \0.*/
+
+	  for (idx = 0; idx < size && (two_dots < (coluna-1)); idx++)
+      if(line[idx] == ':')
+			  two_dots++;
+
+    if (idx == size)
+    	reachEnd = 1;
+  	else {
+    	for (i = 0; line[idx]!=':' && line[idx]!='\n' && line[idx]!='\0'; idx++, i++)
+      	saved_elem[i] = line[idx];
+    	saved_elem[i] = '\0';
+      (*val) = atol(saved_elem);
+    }
+
+    return (reachEnd) ? -1 : idx;
+}
+
+int stackOrg(long *stack, int i, int nlines, long valCol) {
+
+    if (i == nlines) {
+      nlines--;
+      while (nlines > 0) {
+        stack[nlines-1] = stack[nlines];
+        nlines--;
+      }
+      stack[i-1] = valCol;
+    }
+    else {
+    	stack[i] = valCol;
+      i++;
+    }
+
+		return i;
+}
+
+int countDigits(long n){
+
+    int count = 0;
+
+    if (n <= 0){     // contar o sinal -
+        count++;
+    }
+
+    while(n != 0){
+        n /= 10;    // n = n/10
+        count++;
+    }
+
+    return count;
+}
+
+long operate(long *stack, int i, const char* op){
+
+  	int it = 0;
+  	long res = 0;
+
+  	if (i > 0) {
+ 	 		if (!strcmp(op, "avg")) {
+    		for (; it < i; it++)
+    			res += stack[it];
+      	res /= i;
+  		}
+  		else if (!strcmp(op, "sum"))
+    		for (; it < i; it++)
+        	res += stack[it];
+      else if (!strcmp(op, "max")) {
+        res = stack[0];
+      	for (it = 1; it < i; it++)
+          if (stack[it] > res)
+          	res = stack[it];
+      }
+      else if (!strcmp(op, "min")) {
+      	res = stack[0];
+        for (it = 1; it < i; it++)
+          if (stack[it] < res)
+          	res = stack[it];
+      }
+    }
+
+  	return res;
+}
+
+int checkOP(const char* op) {
+
+  	return (!strcmp(op, "avg") || !strcmp(op, "max") || !strcmp(op, "min") || !strcmp(op, "sum"));
+}
+
+/*
+PIPE_BUF = 4096
+
+argv[0] -> ./window
+argv[1] -> número da coluna sobre a qual iremos fazer operações;
+argv[2] -> operação - pode ser 'avg', 'max', 'min' ou 'sum';
+argv[3] -> número máximo de linhas sobre o qual a operação se irá efetuar.
+
+*/
+int main(int argc, char const *argv[]) {
+
+    int x, ins, i = 0;
+  	long valCol, res;
+  	int ncol = atoi(argv[1]);
+    int nlines = atoi(argv[3]);
+    const char* operation = argv[2];
+    char buf[PIPE_BUF];
+    long stack[nlines];
+
+  	if (argc != 4) {
+    	write(1, "Too many or not enough arguments passed to the program.\n", 56);
+      return EXIT_FAILURE;
+  	}
+  	if (!checkOP(operation)) {
+    	write(1, "The operation is not supported by this program. Try 'avg', 'max', 'min' or 'sum'.\n", 82);
+      return EXIT_FAILURE;
+  	}
+    while (x = read(0, buf, PIPE_BUF)) {
+			res = operate(stack, i, operation);
+      if (x + countDigits(res) + 2 > PIPE_BUF) {
+        write(1, "The length of the newly created line exceeds PIPE_BUF and, therefore, is too long.\n", 83);
+				return EXIT_FAILURE;
+      }
+      ins = getVal(buf, ncol, &valCol);
+      if (ins >= 0)
+        i = stackOrg(stack, i, nlines, valCol);
+      sprintf(buf+x-1, ":%ld\n", res);
+    	write(1, buf, strlen(buf));
+    }
+
+    return EXIT_SUCCESS;
+}
