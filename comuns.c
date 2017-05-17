53f6180b25e6b794f1d0e02f04b122ea35612639
Diana <a78985@alunos.uminho.pt>
2017-05-17 16:03:19 +0100

    added functions to comuns

---
+++ comuns.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * Lê uma linha até ao tamanho máximo 'count'(corresponderá a PIPE_BUF).
+ */
+ int read_line(int fd, char *buf, int count) {
+
+	 int endOfLine = 0, nbytes = 0;
+   char c;
+
+	 while (!endOfLine && read(fd, &c, 1) == 1 ) {
+     if (nbytes < count)
+       buf[nbytes] = c;
+     nbytes++;
+     if (c == '\n')
+       endOfLine = 1;
+   }
+	 buf[nbytes] = '\0';
+
+	 return nbytes;
+ }
+
+ 
+ char* read_words(char *buffer){
+ 	char c;
+ 	char **string = malloc(sizeof(char)*10);
+ 	int i, j;
+
+ 	while(buffer[i] != '\n'){
+ 		for(i = 0; buffer[i]!=' '; i++){
+ 		*string[i] = buffer[j];
+ 		}
+ 	*string[i] = '\0';
+ 	i++;
+ 	}
+
+ 	return *string;
+
+}
